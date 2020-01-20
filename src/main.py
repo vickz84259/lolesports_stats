a6fa42c12f7de46faa48b8efcf8a2b8da5143453
Victor Otieno <vickz84259@gmail.com>
2020-01-20 23:50:55 +0300

    Update basic GAE app
    These changes replicate the previous basic Google App Engine application.
    Instead of using flask, this one uses aiohttp.
    
    Signed-off-by: Victor Otieno <vickz84259@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,13 +1,39 @@
-from flask import Flask
+import sys
 
-app = Flask(__name__)
+from aiohttp import web
+from loguru import logger
+
+# Setting up the logger
+format = '{time:YYYY-MM-DD HH:mm:ss ZZ} | {level: <8} | {message}'
+logger.configure(
+    handlers=[
+        dict(sink=sys.stderr, format=format, level='DEBUG')
+    ]
+)
+
+routes = web.RouteTableDef()
 
 
-@app.route('/_ah/start')
-def hello_world():
-    return 'Hello, World!'
+@routes.get('/')
+async def index(request: web.Request) -> web.Response:
+    return web.Response(text='Hello World')
 
 
-@app.route('/')
-def index():
-    return 'Home page'
+@routes.get('/_ah/start')
+async def init(request: web.Request) -> web.Response:
+    logger.info('Service has started.')
+
+    '''
+    game_id = 102844412722519367
+    lol.process_match(game_id)'''
+
+    logger.info('Completed processing.')
+
+    return web.Response()
+
+
+async def web_app() -> web.Application:
+    app = web.Application()
+    app.add_routes(routes)
+
+    return app
